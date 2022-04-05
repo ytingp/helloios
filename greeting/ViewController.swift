05af076736953f8628d2ca8d656664ff09f1f79e
ting <yinting_p@yahoo.com>
2022-04-05 16:36:12 -0700

    added screenshots and text

---
+++ greeting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  greeting
+//
+//  Created by stlp on 3/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
